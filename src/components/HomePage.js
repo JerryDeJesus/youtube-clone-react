30cf0ea99380897e63eda48d86e71f52084064e0
Jerry De Jesus <jdejesus7@live.com>
2021-12-07 11:25:16 -0500

    search bar added

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,14 +5,32 @@ import { Component } from 'react';
 class HomePage extends Component {
   constructor(){
     super()
+    this.state = {
+      searchValue:""
+    }
   }
+
+  handleInput=(event)=>{
+    this.setState({
+      searchValue: event.target.value
+    })
+  }
+
+  handleSubmit=(event)=>{
+    event.preventDefault();
+    this.setState({
+      searchValue:""
+    });
+    console.log("testing");
+  }
+
   render(){
     return (
       <div className="HomePage">
         homepage
         <div>
-            <form>
-                <input type="text" placeholder="Search"></input>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Search" onInput={this.handleInput}></input>
                 <button type="submit">Submit</button>
             </form>
         </div>
