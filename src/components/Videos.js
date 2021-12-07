920f80731926bc600ccecc552800f9953242e304
Jerry <86433443+JerryDeJesus@users.noreply.github.com>
2021-12-07 14:54:55 -0500

    Update Videos.js

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Component } from 'react';
-import { Link, Route } from 'react-router-dom';
+// import { Link, Route } from 'react-router-dom';
 
 class Videos extends Component {
 //   constructor(){
